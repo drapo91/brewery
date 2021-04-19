0a7c396e000932c0f64a75309ab3c92ee28d6b42
drapo <drapo91@gmail.com>
2021-04-19 22:00:23 +0200

    Adding Beer date mapper

--- src/main/java/com/drapo/brewery/web/mappers/BeerMapper.java
+++ src/main/java/com/drapo/brewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import com.drapo.brewery.domain.Beer;
 import com.drapo.brewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = DateMapper.class)
 public interface BeerMapper {
     BeerDto beerToBeerDto(Beer beer);
     Beer beerDtoToBeer(BeerDto beerDto);
---
+++ src/main/java/com/drapo/brewery/web/mappers/DateMapper.java
@@ -0,0 +1,30 @@
+package com.drapo.brewery.web.mappers;
+
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Component
+public class DateMapper {
+
+    public OffsetDateTime asOffsetDateTime(Timestamp ts) {
+        if (ts != null) {
+            return OffsetDateTime.of(ts.toLocalDateTime().getYear(), ts.toLocalDateTime().getMonthValue(),
+                    ts.toLocalDateTime().getDayOfMonth(), ts.toLocalDateTime().getHour(), ts.toLocalDateTime().getMinute(),
+                    ts.toLocalDateTime().getSecond(), ts.toLocalDateTime().getNano(), ZoneOffset.UTC);
+        } else {
+            return null;
+        }
+    }
+
+    public Timestamp asDate(OffsetDateTime offsetDateTime) {
+        if (offsetDateTime != null) {
+            return Timestamp.valueOf(offsetDateTime.atZoneSameInstant(ZoneOffset.UTC).toLocalDateTime());
+        } else {
+            return null;
+        }
+    }
+}
