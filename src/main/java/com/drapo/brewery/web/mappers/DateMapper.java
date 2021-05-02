b7d849722d0d474a6be2c3db9e73d6c586629daf
drapo <drapo91@gmail.com>
2021-05-02 11:21:56 +0200

    refactor

--- src/main/java/com/drapo/brewery/web/mappers/DateMapper.java
+++ src/main/java/com/drapo/brewery/web/mappers/DateMapper.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 import java.sql.Timestamp;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
 
 @Component
 public class DateMapper {
